4bcec657d0a8e83871a98852625a39c664b42806
Sylvain NGO <39770225+az-vi95@users.noreply.github.com>
2020-04-28 21:19:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ class User
      * attribut de classe avec une valeur par défaut
      * @var string 
      */
-    public $firstname = 'Jax'; //  $firstname est une variable
+    public $firstname = 'Jax1'; //  $firstname est une variable
 
     /**
      *  attribut sans valeur par défaut (=null)
